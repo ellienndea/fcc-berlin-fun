4fe1ad3fa0cdef0114011f0b33451e553ed47726
SammyShehter <sammyshehter@gmail.com>
2022-01-30 21:59:53 +0200

    Sammy #comment restructured menu and index.html

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-const btn_open_menu = document.getElementById("btn-open-menu");
-const stripe_1 = document.querySelector(".stripe1");
-const stripe_2 = document.querySelector(".stripe2");
-const stripe_3 = document.querySelector(".stripe3");
-const menu = document.getElementById("menu");
+'use strict'
 
-function toggleMenu(){
-    stripe_1.classList.toggle("menu-closed");
-    stripe_2.classList.toggle("menu-closed");
-    stripe_3.classList.toggle("menu-closed");
-    menu.classList.toggle("hide-menu");
-    menu.classList.toggle("show-menu");
+const burger_menu = document.querySelector('.burger-menu')
 
-    let pressed = (btn_open_menu.getAttribute("aria-pressed") === "true");
-   /* Change aria-pressed to the opposite state */
-    btn_open_menu.setAttribute("aria-pressed", !pressed);
-}
-
-/* time-out for theme toggle btn and menu open-close btn */
-btn_open_menu.addEventListener("click", smallTimeOut);
-/* https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/button#disabling_and_enabling_a_button */
-function smallTimeOut(){
-    this.disabled = true;
-    window.setTimeout(() => this.disabled = false, 1500);
-}
-/* close menu after click on menu item */
-menu.addEventListener("click", toggleMenu);
+burger_menu.addEventListener('click', function() {
+    this.classList.toggle("menu-expanded");
+})
