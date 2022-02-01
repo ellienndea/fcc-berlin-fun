eb903e00143ae09d90d3d193e6cdfe6afd1c856c
SammyShehter <sammyshehter@gmail.com>
2022-02-01 16:55:22 +0200

    Style-the-menu-in-index #comment changed js Query selector, merged conflicts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const burger_menu = document.querySelector('.burger-menu')
+const burger_menu = document.querySelector('.burger_menu')
 
 burger_menu.addEventListener('click', function() {
     this.classList.toggle("menu-expanded");
